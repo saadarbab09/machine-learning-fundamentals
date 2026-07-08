192ddfb9fa2a0377689b36fcb49c0da82659353d
Saad Arbab <saadarbab9@gmail.com>
2026-07-08 02:32:03 +0100

    Added Logistic Regression, SVM, and Neural Network classifier

--- magicML.py
+++ magicML.py
@@ -6,6 +6,9 @@ from imblearn.over_sampling import RandomOverSampler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+import tensorflow as tf
 
 
 
@@ -17,6 +20,7 @@ df['class'] = (df['class'] == 'g').astype(int)
 
 # CREATING HISTOGRAMS FOR EACH FEATURE
 for label in columns[:-1]:
+    plt.figure()
     plt.hist(df[df['class']==1][label], color = 'blue', alpha = 0.7, label = 'gamma', density = True)
     plt.hist(df[df['class']==0][label], color = 'red', alpha = 0.7, label = 'hadron', density = True)
     plt.title(label)
@@ -172,4 +176,122 @@ y_predicted_nb = nb_model.predict(X_test) #predict the class labels for the test
 #     accuracy                           0.73      3804
 #    macro avg       0.72      0.66      0.67      3804
 # weighted avg       0.73      0.73      0.71      3804
-    
+
+
+
+
+ #***** (3) LOGISTIC REGRESSION *****
+ # this model  uses y = mx + c to derive the sigmoid fuction.
+ # which outputs the probability of a sample belonging to a class.
+ # the sigmoid function is defined as: 1 / (1 + e^(-z)).
+
+
+ # IMPLEMENTATION:
+# from sklearn.linear_model import LogisticRegression
+lr_model = LogisticRegression()# as default it uses L2 penalty which is quadratic: penalises outliers heavily.
+lr_model = lr_model.fit(X_train, y_train) #fit the model to the training data
+y_predicted_lr = lr_model.predict(X_test) #predict the class labels for the test
+# print(classification_report(y_test, y_predicted_lr)) #print the classification report LR model.
+
+
+##### LOGISTIC REGRESSION CLASSIFICATION REPORT:
+#               precision    recall  f1-score   support
+
+#            0       0.67      0.71      0.69      1338
+#            1       0.84      0.81      0.82      2466
+
+#     accuracy                           0.77      3804
+#    macro avg       0.75      0.76      0.76      3804
+# weighted avg       0.78      0.77      0.77      3804
+
+
+
+ #***** (4) SUPPORT VECTOR MACHINE (SVM) *****
+ # this model finds the best hyperplane that best divides the classes in the feature space.
+ # it also maximizes the margin between the svm hyperplane and support vectors
+ # which helps improve generalization and reduce overfitting. 
+ #for the feature space that is not linearly separable.
+ # it uses kernel trick to map data into a higher dimensional space that is seperable.
+
+
+ # IMPLEMENTATION:
+# from sklearn.svm import SVC
+svm_model = SVC()
+svm_model = svm_model.fit(X_train, y_train) #fit the model to the training data
+y_predicted_svm = svm_model.predict(X_test) #predict the class labels for the test
+# print(classification_report(y_test, y_predicted_svm))  classification report
+
+
+##### SVM CLASSIFICATION REPORT:
+#               precision    recall  f1-score   support
+
+#            0       0.83      0.79      0.81      1347
+#            1       0.89      0.91      0.90      2457
+
+#     accuracy                           0.87      3804
+#    macro avg       0.86      0.85      0.85      3804
+# weighted avg       0.86      0.87      0.86      3804
+
+
+
+
+
+#***** (4) NEURAL NETWORK FOR CLASSIFICATION *****
+# this model uses multiple layers of interconnected nodes to learn complex patterns in the data.
+# it uses activation functions to remove linearity. sigmoid, relu, tanh are some of the activation functions.
+# and uses gradient descent backpropagation to update weights for adjusting the model to minimize the loss function.
+# we will use the tesorflow library to implement this model.
+
+#IMPLEMENTATION:
+# import tensorflow as tf
+nn_model = tf.keras.Sequential([
+    tf.keras.Input(shape=(10,)),
+    tf.keras.layers.Dense(32, activation='relu'),# start with 32 n layer , define activation func and tell input shape number of features in the input data. 
+    tf.keras.layers.Dense(32, activation='relu'), # second layer with 32n
+    tf.keras.layers.Dense(1, activation='sigmoid') # condense it to 1 node , with sigmoid for classification.
+
+])
+# to configure the model we use .compile(), which tells how to:
+# update weights, measure errors, what performance metric to report  
+nn_model.compile(optimizer = tf.keras.optimizers.Adam(0.001), loss = 'binary_crossentropy', metrics = ['accuracy'])
+#optimizer is the algorithm that updates the weights during backpropogation, learning rate alpha = 0.001
+#(a smarter version of gradient descent that auto adjusts and updates )
+# loss = 'binarycrossentropy' is a loss function to model loss 
+# metrics = ['accuracy'] not for training. keras calculates and records the accuracy with each cycle epoch
+# to visualise these stats and accuracy well use tensorflow plots
+#to plot the loss over each epoch/training cycle:
+def plot_loss(history):
+    plt.figure()
+    plt.plot(history.history['loss'], label = 'loss')
+    plt.plot(history.history['val_loss'], label = 'val_loss')
+    plt.xlabel('Epoch')
+    plt.ylabel('Binary crossentropy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+#to plot the accuracy over each epoch/training cycle:
+def plot_accuracy(history):
+    plt.figure()
+    plt.plot(history.history['accuracy'], label = 'accuracy')
+    plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+    plt.xlabel('Epoch')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.grid(True)
+    plt.show()   
+
+
+### TRAINING THE MODEL ###
+history = nn_model.fit(
+    X_train,
+    y_train,
+    epochs=100,
+    batch_size=32,
+    validation_split=0.2,
+    verbose=1
+) # bathsize means intead of taking all the sample togoether take in batches of 32 
+    # Training is done in batches of 32 samples. After each batch, the model makes predictions,
+# computes the loss, performs backpropagation, and updates the weights.
+# This continues until all training samples have been processed once, which completes one epoch.-    
+plot_loss(history)
+plot_accuracy(history)
